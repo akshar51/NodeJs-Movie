68a38c6ec1f9c37e38a356f93dc609426f373d94
akshar51 <aksharparekh401@gmail.com>
2025-08-19 22:50:09 +0530

    done

---
+++ assets/assets/js/screen.js
@@ -0,0 +1,51 @@
+const seatPrice = 200; 
+let selectedSeats = [];
+let bookings = [];
+
+const checkoutBtn = document.getElementById("checkoutBtn");
+const seatElements = document.querySelectorAll(".seat");
+const toastLiveExample = document.getElementById('liveToast')
+const AtleastOneSeat = document.getElementById('seatSelect')
+
+
+    // Handle seat selection
+seatElements.forEach(seat => {
+      seat.addEventListener("click", () => {
+        const seatNumber = seat.dataset.seat;
+
+        if (selectedSeats.includes(seatNumber)) {
+          // Remove if already selected
+          selectedSeats = selectedSeats.filter(s => s !== seatNumber);
+          seat.classList.remove("selected");
+        } else {
+          selectedSeats.push(seatNumber);
+          seat.classList.add("selected");
+        }
+      });
+    });
+
+// checkout
+checkoutBtn.addEventListener("click", () => {
+      if (selectedSeats.length === 0) {
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(AtleastOneSeat);
+        toastBootstrap.show();
+        return;
+      }
+
+      let booking = {
+        seats: [...selectedSeats],  
+      };
+
+      // Push to booking history
+      bookings.push(booking);
+
+      console.log("Bookings:", bookings);
+
+      // Reset selection for new user
+      selectedSeats = [];
+      seatElements.forEach(seat => seat.classList.remove("selected"));
+
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastBootstrap.show();
+    });
+
