895d0d6a9de506fe23fc06a445dd231602b3aae1
akshar51 <aksharparekh401@gmail.com>
2025-08-19 21:30:20 +0530

    done

---
+++ assets/assets/js/movie.js
@@ -0,0 +1,8 @@
+ const dateButtons = document.querySelectorAll(".date-btn");
+
+    dateButtons.forEach(btn => {
+      btn.addEventListener("click", () => {
+        dateButtons.forEach(b => b.classList.remove("selected"));
+        btn.classList.add("selected");
+      });
+    });
