da7ea5bd3c5c862bdb4a5423a7cdc50ba66a3f61
akshar51 <aksharparekh401@gmail.com>
2025-08-20 12:55:14 +0530

    done

--- assets/assets/js/screen.js
+++ assets/assets/js/screen.js
@@ -33,7 +33,10 @@ checkoutBtn.addEventListener("click", () => {
       }
 
       let booking = {
-        seats: [...selectedSeats],  
+        seats: [...selectedSeats],
+        price : seatPrice * selectedSeats.length,
+        date: dateSelect.value,
+      time: timeSelect.value  
       };
 
       // Push to booking
@@ -44,8 +47,30 @@ checkoutBtn.addEventListener("click", () => {
       // Reset selection for new user
       selectedSeats = [];
       seatElements.forEach(seat => seat.classList.remove("selected"));
+      dateSelect.value = "";
+  timeSelect.value = "";
 
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
     toastBootstrap.show();
     });
 
+// date & time
+const dateSelect = document.getElementById("showDate");
+const timeSelect = document.getElementById("showTime");
+
+function populateDates() {
+  const today = new Date();
+  for (let i = 0; i <= 2; i++) {
+    const date = new Date();
+    date.setDate(today.getDate() + i);
+
+    const options = { weekday: 'short', day: 'numeric', month: 'short' };
+    const formatted = date.toLocaleDateString(undefined, options);
+
+    const opt = document.createElement("option");
+    opt.value = date.toISOString().split("T")[0]; // yyyy-mm-dd
+    opt.textContent = formatted;
+    dateSelect.appendChild(opt);
+  }
+}
+populateDates();
